ff42be3846d809ff31cb0425456f0d89b7952210
Sarath Kaul <kaul.sarath@gmail.com>
2020-05-30 10:49:25 +0000

    Package Changes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(session({secret:'SARATH'}));
 const route = require('./routes/route');
 
 //connect to mongodb
-mongoose.connect("mongodb+srv://skaul05:kL1032V0o4270O8l@cluster0-rs7nr.gcp.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect(process.env.mongodb_uri || 'mongodb://localhost:27017/SIS');
 
 //on connection
 mongoose.connection.on('connected',()=>{
